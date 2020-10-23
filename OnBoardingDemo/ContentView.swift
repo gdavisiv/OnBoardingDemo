2a76c144ef970edc9645a11988ca1f6623ea8f95
George Davis IV <gdavisiv@gmail.com>
2020-10-22 21:57:10 -0400

    Initial Commit

---
+++ OnBoardingDemo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  OnBoardingDemo
+//
+//  Created by George Davis IV on 10/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
